511f1ce232def78d672024deb33deb8562a33c44
Yongchang <yongchang.v.zhang@gmail.com>
2023-10-19 22:00:24 +1100

    add find_proper_divisors.py

---
+++ find_proper_divisors.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Determine all real divisors of a number.
+"""
+
+import sys
+
+def find_proper_divisors(number):
+    divisors = [1]
+    for i in range(2, number // 2 + 1):
+        if number % i == 0:
+            divisors.append(i)
+    return divisors
+
+def main():
+    if len(sys.argv) != 2:
+        print("One and only one argument expected.")
+        raise SystemExit(2)
+    try:
+        number = int(sys.argv[1])
+        print(find_proper_divisors(number))
+    except:
+        print("The argument should be an integer.")
+
+if __name__ == "__main__":
+    main()
