a6270867ee7be14ffa8834b318ad8396de14293b
Yongchang <yongchang.v.zhang@gmail.com>
2023-10-20 21:32:22 +1100

    update find_proper_divisors.py with list comprehension approach

--- find_proper_divisors.py
+++ find_proper_divisors.py
@@ -5,12 +5,17 @@ Determine all real divisors of a number.
 
 import sys
 
+# def find_proper_divisors(number):
+#     divisors = [1]
+#     for i in range(2, number // 2 + 1):
+#         if number % i == 0:
+#             divisors.append(i)
+#     return divisors
+
+# List comprehension approach
 def find_proper_divisors(number):
-    divisors = [1]
-    for i in range(2, number // 2 + 1):
-        if number % i == 0:
-            divisors.append(i)
-    return divisors
+    proper_divisors = [i for i in range(2, number // 2 + 1) if number % i == 0]
+    return proper_divisors
 
 def main():
     if len(sys.argv) != 2:
